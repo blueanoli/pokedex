3201bebc900304f4d2fa934b3cc7bf5106ad137f
ILONA KOSCHI <ILONA.KOSCHI@HOTMAIL.com>
2024-02-28 16:40:38 +0100

    added renderPokedex function added more style added renderHTML Pokecard

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 let pokemon;
-
-let NORMAL_COLOR = '#AAAA99';
-let FIGHTING_COLOR = '#BB5545';
-let FLYING_COLOR = '#8899FF';
-let POISON_COLOR = '#AA5599';
-let GROUND_COLOR = '#DDBB55';
-let ROCK_COLOR = '#BBAA66';
-let BUG_COLOR = '#AABB22';
-let GHOST_COLOR = '#6667BC';
-let FIRE_COLOR = '#EC4225';
-let WATER_COLOR = '#4E9AFF';
-let GRASS_COLOR = '#77CC55';
-let ELECTRIC_COLOR = '#F5CC34';
-let PSYCHIC_COLOR = '#EE5499';
-let ICE_COLOR = '#66CCFF';
-let DRAGON_COLOR = '#7867EE';
-
 let pokedex = [];
-let id = [];
+const TYPE_COLORS = {
+    normal: '#AAAA99',
+    fighting: '#BB5545',
+    flying: '#8899FF',
+    poison: '#AA5599',
+    ground: '#DDBB55',
+    rock: '#BBAA66',
+    bug: '#AABB22',
+    ghost: '#6667BC',
+    fire: '#EC4225',
+    water: '#4E9AFF',
+    grass: '#77CC55',
+    electric: '#F5CC34',
+    psychic: '#EE5499',
+    ice: '#66CCFF',
+    dragon: '#7867EE'
+};
 
 async function loadPokemon() {
     let url = `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151`;
@@ -25,28 +24,63 @@ async function loadPokemon() {
     pokemon = await response.json();
     console.log(pokemon);
     renderPokedex();
-    //renderPokemonInfo(); // --> JUNUS CODE
 }
-
-// function renderPokemonInfo() { // --> JUNUS CODE
-//     document.getElementById('pokemon-name').innerHTML = pokemon['name'].charAt(0).toUpperCase() + pokemon['name'].slice(1);
-//     document.getElementById('pokemon-id').innerHTML = pokemon['id'];
-//     document.getElementById('pokemon-image').src = pokemon['sprites']['other']['dream_world']['front_default'];
-// }
-
 async function renderPokedex() {
     document.getElementById('main-container').innerHTML = '';
     pokedex = pokemon['results'];
-    // let pokeURL = `https://pokeapi.co/api/v2/pokemon/0/`;
-    // let response = await fetch(pokeURL);
-    // pokeData = await response.json();
-    // console.log(pokeData);
 
     for (let i = 0; i < pokedex.length; i++) {
+        let pokeURL = pokedex[i].url;
+        let response = await fetch(pokeURL);
+        let pokeData = await response.json();
+
+        let pokemonType = pokeData['types'][0]['type']['name'];
+        let bgColor = TYPE_COLORS[pokemonType];
+
         document.getElementById('main-container').innerHTML += /*html*/ `
-            <div id="pokemon">
-                <h1>${pokemon['results'][i]['name'].charAt(0).toUpperCase() + pokemon['results'][i]['name'].slice(1)}</h1>
-            </div>
-        `;
+            <div class="pokemon" style="background-color: ${bgColor};" onclick="showPokemon(${pokeData['id']})">
+                <div class="headline">
+                    <h1>${pokeData['name'].charAt(0).toUpperCase() + pokeData['name'].slice(1)}</h1>
+                    <h1>${pokeData['id']}</h1>
+                </div>
+                <div class="types">
+                    <p>Type: ${pokeData['types'][0]['type']['name'].charAt(0).toUpperCase() + pokeData['types'][0]['type']['name'].slice(1)}</p>
+                </div>
+                <img src="${pokeData['sprites']['other']['dream_world']['front_default']}" alt="${pokeData.name}">
+            </div>`;
     }
+}
+
+async function showPokemon(id) {
+    let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    let response = await fetch(url);
+    let pokeData = await response.json();
+    
+    renderPokeCard(pokeData);
+}
+// RENDER HTML -----------------------------------------------------------------------------------------------------------
+
+function renderPokeCard(pokeData){
+    document.getElementById('pokedex').innerHTML = '';
+    document.getElementById('pokedex').innerHTML += /*html*/ `
+        <div class="pokedex" style="background-color: ${TYPE_COLORS[pokeData['types'][0]['type']['name']]};">
+            <div id="pokemon-name">
+                <h1>${pokeData['name'].charAt(0).toUpperCase() + pokeData['name'].slice(1)}</h1>
+                <h1>${pokeData['id']}</h1>
+            </div>
+            <div class="types">
+                <p>Type: ${pokeData['types'][0]['type']['name'].charAt(0).toUpperCase() + pokeData['types'][0]['type']['name'].slice(1)}</p>
+            </div>
+            <img id="pokemon-image" src="${pokeData['sprites']['other']['dream_world']['front_default']}" alt="${pokeData.name}">
+        </div>
+        <div class="info-container">
+            <h2>Stats</h2>
+            <p>HP: ${pokeData['stats'][0]['base_stat']}</p>
+            <p>Attack: ${pokeData['stats'][1]['base_stat']}</p>
+            <p>Defense: ${pokeData['stats'][2]['base_stat']}</p>
+            <p>Special Attack: ${pokeData['stats'][3]['base_stat']}</p>
+            <p>Special Defense: ${pokeData['stats'][4]['base_stat']}</p>
+            <p>Speed: ${pokeData['stats'][5]['base_stat']}</p>
+        </div>
+    `;
 }
